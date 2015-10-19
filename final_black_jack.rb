ec2ee2c7300e89ebcdf0129ff3aed90d7ad48957
Tyler Yagman <tyagman@gmail.com>
2015-10-19 15:37:18 -0400

    repl game

---
+++ final_black_jack.rb
@@ -0,0 +1,61 @@
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
+def deal_card
+  rand(1..11)
+end
+
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+end
+
+def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
+end
+
+def get_user_input
+  gets.chomp
+end
+
+def end_game(card_total)
+  puts "Sorry, you got #{card_total}. Thanks for playing!"
+end
+
+def initial_round
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
+end
+
+def hit? (card_total)
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+  end
+end
+
+def invalid_command
+  puts "Sorry, not a valid command"
+  get_user_input
+end
+
+def play
+ welcome
+  total = initial_round
+  until total > 21
+    current_total = total
+    total = hit? (current_total)
+    # break if total == current_total
+    display_card_total(total)
+  end
+  end_game(total)
+
+end
+
+play
